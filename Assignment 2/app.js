023ae8864d2e50346dbc98e3c2da5f2b54b53e15
Moizkazmi245 <abdul.moiz.kazmi@gmail.com>
2024-09-12 16:31:49 +0500

    almost complete

--- Assignment 2/app.js
+++ Assignment 2/app.js
@@ -23,3 +23,62 @@ function showMessage() {
     alert(titledMessage);
     console.log(titledMessage);
 }
+
+
+// 4. Write a script to save student’s bio data in JS variables and 
+// show the data in alert boxes.
+
+
+function showBio() {
+    var stuName = "Ayan Zahid";
+    var stuFatherName = "Zahid";
+    var certification = "Web and App Developement 1 year"
+
+    alert("Name:\n" + stuName)
+    alert("Father Name:\n" + stuFatherName)
+    alert("Certified:\n" + certification)
+}
+
+
+// 5. Write a script to display the following alert using one JS 
+// variable:
+
+function oneJsVariable() {
+
+    var pizzaMesg = "PIZZA\nPIZZ\nPIZ\nPI\nP"
+    alert(pizzaMesg)
+}
+
+// 6. Declare a variable called email and assign to it a string that 
+// represents your Email Address(e.g. example@example.com). 
+// Show the blow mentioned message in an alert box.(Hint: use 
+// string concatenation)
+
+function showEmail() {
+    var emailAddress = "abdul.moiz.kazmi@gmail.com"
+    alert("Email:" + emailAddress)
+    console.log(emailAddress);
+
+}
+
+// 7. Declare a variable called book & give it the value “A 
+// smarter way to learn JavaScript”. Display the following 
+// message in an alert box:
+
+
+function book() {
+    var bookMesg = "A smarter way to learn JavaScript"
+    alert(bookMesg)
+    console.log(bookMesg);
+
+}
+
+
+// 8. Write a script to display this in browser through JS 
+
+var showMessage1 = document.querySelector("#showMessage"); 
+
+function showMesg (){
+    var mesg = "Asslam O Alikom! whats up bro"
+    showMessage1.innerHTML = mesg;
+}
