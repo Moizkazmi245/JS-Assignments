2dc39fa770b317aa435c34275b41be0a55e70c77
Moizkazmi245 <abdul.moiz.kazmi@gmail.com>
2024-09-12 15:59:15 +0500

    assignmetns

---
+++ Assignment 1/app.js
@@ -0,0 +1,43 @@
+// 1. Write a script to greet your website visitor using JS alert box.
+
+var mesg1 = alert("Assalam o alikom brother")
+console.log(mesg1);
+
+
+//2. Write a script to display following message on your web page: 
+
+var mesg2 = alert("Assalam o Alikom brother welcome to our website")
+console.log(mesg2);
+
+
+//3. Write a script to display following message on your web 
+// page: (Hint : Use line break)
+
+var mesg3 = alert("Assalam o Alikom brother. \nWe hope you are enjoing this website");
+console.log(mesg3);
+
+
+//3. Write a script to display following messages in sequence:
+
+var mesg4 = alert("Welcome to Js Island");
+console.log(mesg4);
+
+var mesg5 = alert("Wohoo!");
+console.log(mesg5);
+
+
+//4. Generate the following message through browser’s 
+// developer console: 
+
+var mesg6 = alert("Assalam o Alikom brother welcome to our website")
+console.log(mesg6);
+
+console.log("Assalam o Alikom brother. We hope you are enjoying this website.");
+
+
+// 6. Make use of alerts in your new/existing HTML & CSS 
+// project.
+
+function showAlert() {
+    alert("This is an alert box! Welcome to my HTML & CSS project.");
+}
---
+++ Assignment 2/app.js
@@ -0,0 +1,25 @@
+// 1.Declare a variable called username.
+var usernmae = "Ali Abrar"
+console.log(usernmae);
+
+
+// 2. Declare a variable called myName & assign to it a string 
+// that represents your Full Name. 
+
+var myName = "Syed Moiz Kazmi"
+alert(myName)
+console.log(myName);
+
+
+// 3. Write script to 
+// a) Declare a JS variable, titled message. 
+// b) Assign “Hello World” to variable message 
+// c) Display the message in alert box.
+
+var titledMessage = "Assalam O Alikom! ! hop you are fine"
+
+function showMessage() {
+
+    alert(titledMessage);
+    console.log(titledMessage);
+}
